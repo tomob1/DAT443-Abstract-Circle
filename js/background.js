5fd0e4fcb5c28dcf27f47a8894241120dad9ba6e
AirTMZ <50797752+AirTMZ@users.noreply.github.com>
2023-01-12 20:05:01 +0000

    Update background.js

--- js/background.js
+++ js/background.js
@@ -1,8 +1,8 @@
 var images = []; 
-var imageFiles = fs.readdirSync("/backgrounds"); 
+var imageFiles = fs.readdirSync("js/backgrounds"); 
 
 for (var i = 0; i < imageFiles.length; i++) { 
-  images.push("/backgrounds" + imageFiles[i]); 
+  images.push("js/backgrounds" + imageFiles[i]); 
 }
 
 var randomNumber = Math.floor(Math.random() * images.length); 
