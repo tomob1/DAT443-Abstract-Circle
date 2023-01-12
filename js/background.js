2319f545a2b31b8503cccb217594287cf9daebdd
AirTMZ <50797752+AirTMZ@users.noreply.github.com>
2023-01-12 19:54:37 +0000

    Create background.js

---
+++ js/background.js
@@ -0,0 +1,10 @@
+var images = []; 
+var imageFiles = fs.readdirSync("/path/to/folder"); 
+
+for (var i = 0; i < imageFiles.length; i++) { 
+  images.push("/backgrounds" + imageFiles[i]); 
+}
+
+var randomNumber = Math.floor(Math.random() * images.length); 
+
+document.body.style.backgroundImage = "url(" + images[randomNumber] + ")";
